ab253c46dac2962ab07cd143a0a8514f038534cb
kartikay439 <kartikaytrivedi99@gmail.com>
2025-04-18 12:56:57 +0530

    routes added

---
+++ question1/index.js
@@ -0,0 +1,12 @@
+import express from "express"
+import router from "./routes/user.route";
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+app.use("/", router);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port:${PORT}`);
+});
